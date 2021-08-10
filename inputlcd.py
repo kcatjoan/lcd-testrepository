fe13f844461eb5908e8e779d9fda8c2a3a402097
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-10 18:42:29 -0400

    Update inputlcd.py
    copied the word-break stuff from wordbreak.py and replaced instances of print with message=

--- inputlcd.py
+++ inputlcd.py
@@ -5,7 +5,7 @@ import time
 import os.path 
 from os import path
 
-import Adafruit_CharLCD as LCD
+  import Adafruit_CharLCD as LCD
 
 import sys
 # Raspberry Pi pin configuration:
@@ -22,8 +22,7 @@ lcd_columns = 16
 lcd_rows    = 2
 
 # Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 lcd.clear()
   
@@ -37,30 +36,23 @@ if (sys.argv[1] == "clear"):
   lcd.clear()
 if (sys.argv[1] == "repeat"):
   while "rolodex":
-    for x in rolodex:
-      #pause while date displays
-      while (path.exists("/tmp/lock")):
-       time.sleep(1)
-      #check that message will fit under 34 chars
-      if len(x) < 34:
-      #check if message needs breaking (is longer than 16 chars)
-          if len(x) > 16:
-              midpoint = 8
-              #find the first space after the midpoint 
-              space = x.find(' ', midpoint)
-              last = x[space+1:]
-            #if the last bit is too long to display (over 16 chars), the midpoint needs to be earlier so that it can find a sooner space
-              while len(last) > 16:
-                  midpoint = midpoint-1
-                  space = x.find(' ', midpoint)
-                  last = x[space+1:]
-              first = x[:space]
-              potato = first + "\n" + last
-          else:
-              potato = x
-      else:
-          potato = (len(x))
+    rolodex = sys.stdin.readlines()
+for x in rolodex:
+    word = x
+if len(word) < 34:
+    if len(word) > 16:
+        midpoint = 8
+        while len(word[midpoint:]) > 16:
+            midpoint = midpoint+1
+        space = word.find(' ', midpoint)
+        last = word[space+1:]
+        first = word[:space]
+        message = first + "\n" + last
+    else:
+       message = word
+else:
+    message = (len(word))
       #i have tested the message-breaking chunk and it works. sets the variable message to the desired output
 lcd.clear()
-lcd.message(potato.rstrip())
+lcd.message(message.rstrip())
 time.sleep(5)

400114bf0d4d158bbf13e7ea84e501013ebf5691
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-10 19:02:01 -0400

    Update inputlcd.py

--- inputlcd.py
+++ inputlcd.py
@@ -33,7 +33,7 @@ rolodex = sys.stdin.readlines()
 if (sys.argv[1] == "clear"):
   lcd.clear()
 else:
-  lcd.message(sys.argv[1])
+  print(sys.argv[1])
 if (sys.argv[1] == "repeat"):
   while "rolodex":
     rolodex = sys.stdin.readlines()

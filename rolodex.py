a48831fae8eca225870b002e688198a93e5a71e8
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-11 11:20:04 -0400

    Update rolodex.py

--- rolodex.py
+++ rolodex.py
@@ -7,27 +7,34 @@ LETTING THE DAYS GO BY
 LETTING THE WATER HOLD ME DOWN
 LETTING THE DAYS GO BY 
 WATER FLOWING UNDERGROUND
-i heard a bird sing in the dark of december
-a magical thing and sweet to remember
-we are nearer to spring than we were in september
-i heard a bird sing in the dark of december
+i heard a bird sing 
+in the dark of december
+a magical thing 
+and sweet to remember
+we are nearer to spring 
+than we were in september
+i heard a bird sing 
+in the dark of december
 now i think heartbreaking but
 also insane. also very funny.
-this place could be beautiful, right?
-you could make this place beautiful
-BRO! TELL ME WE STILL KNOW HOW TO SPEAK OF KINGS!
-What is more generous than a window?
+this place could be beautiful
+youcouldmake this placebeautiful
+BRO! TELL ME WE STILL 
+KNOW HOW TO SPEAK OF KINGS!
+Whatismore generous thanawindow?
 if i rhyme it is it poetry?
 noetry
 We can hold all these ideas at once
-and feel both heavy and weightless
+andfeelboth heavy and weightless
 with the absurd beauty of it all
-What do you really want from a poem? 
-Is it worth the risk of dying to find out?
+What do you want from a poem? 
+Is it worth dying to find out?
 Some bit of historical bunk
 Filled our grammar with Latinate junk.
 It banned prepositions
 From final positions,
 Ita Anglis difficile nunc.
-Make the ordinary come alive for them.
-The extraordinary will take care of itself.
+Make the ordinary 
+come alive for them.
+The extraordinary will 
+take care of itself.

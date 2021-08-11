b67e5bf93b8f052f9040f7096fdbf0c624adecf4
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-11 11:01:41 -0400

    Update rolodex.py
    put all the stuff back in whee

--- rolodex.py
+++ rolodex.py
@@ -1,5 +1,33 @@
-LETTING THE WATER HOLD ME DOWN
-print
-list
-frog
+IS IT AN OPPORTUNITY 
+nine-lived and contradictory
+123456789 123456789 123456789 123456789
+THIS IS NOT MY BEAUTIFUL HOUSE
+THIS IS NOT MY BEAUTIFUL WIFE
 LETTING THE DAYS GO BY
+LETTING THE WATER HOLD ME DOWN
+LETTING THE DAYS GO BY 
+WATER FLOWING UNDERGROUND
+i heard a bird sing in the dark of december
+a magical thing and sweet to remember
+we are nearer to spring than we were in september
+i heard a bird sing in the dark of december
+now i think heartbreaking but
+also insane. also very funny.
+this place could be beautiful, right?
+you could make this place beautiful
+BRO! TELL ME WE STILL KNOW HOW TO SPEAK OF KINGS!
+What is more generous than a window?
+if i rhyme it is it poetry?
+noetry
+We can hold all these ideas at once
+and feel both heavy and weightless
+with the absurd beauty of it all
+What do you really want from a poem? 
+Is it worth the risk of dying to find out?
+Some bit of historical bunk
+Filled our grammar with Latinate junk.
+It banned prepositions
+From final positions,
+Ita Anglis difficile nunc.
+Make the ordinary come alive for them.
+The extraordinary will take care of itself.

71cc0e791ba882c29c7aa5821a900043b07354ed
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-11 11:44:49 -0400

    Update rolodex.py

--- rolodex.py
+++ rolodex.py
@@ -1,6 +1,5 @@
 IS IT AN OPPORTUNITY 
 nine-lived and contradictory
-9
 THIS IS NOT MY BEAUTIFUL HOUSE
 THIS IS NOT MY BEAUTIFUL WIFE
 LETTING THE DAYS GO BY
@@ -11,8 +10,8 @@ i heard a bird sing
 in the dark of december
 a magical thing 
 and sweet to remember
-we are nearer to spring 
-than we were in september
+"we are nearer to spring 
+than we were in september"
 i heard a bird sing 
 in the dark of december
 now i think 
@@ -31,7 +30,7 @@ with the absurd beauty of itall
 What do you want from a poem? 
 Isit worth dying to find out?
 Some bit of historical bunk
-Fillsour grammer w Latinate junk
+Fillsour grammar w Latinate junk
 It banned prepositions
 From final positions,
 Ita Anglis difficile nunc.

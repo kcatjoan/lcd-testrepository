a255e41bc62d42d645d8216df612f7970559bb67
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-08-10 20:13:23 -0400

    Update inputlcd.py
    fixed indentation

--- inputlcd.py
+++ inputlcd.py
@@ -47,12 +47,12 @@ if len(word) < 34:
         message = first + "\n" + last
     else:
          message = word
-  print(message)
-  lcd.message(message)
-  time.sleep(5)
-  if (sys.argv[1] == "clear"):
+    print(message)
+    lcd.message(message)
     time.sleep(5)
-    lcd.clear()
+    if (sys.argv[1] == "clear"):
+      time.sleep(5)
+      lcd.clear()
 else:
     message = (len(word))
 #between here and prev comment should set the correct message
